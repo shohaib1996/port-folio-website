cfb3f864b48490486bb6269bfbbd7c5a04f56a3e
shohaib1996 <khanshohaibhossain@gmail.com>
2023-12-11 22:26:11 +0600

    everything added

--- components/Header.js
+++ components/Header.js
@@ -7,9 +7,9 @@ import logo from "../public/logo.png"
 
 const Header = () => {
   return (
-    <header className="absolute z-30 w-full flex items-center px-16 xl:px-0 xl:h-[80px]">
+    <header className="absolute z-30 w-full flex items-center px-12 xl:px-0 xl:h-[80px]">
       <div className="container mx-auto">
-        <div className="flex flex-col lg:flex-row justify-between items-center gap-y-6 py-8">
+        <div className="flex flex-col lg:flex-row justify-between items-center gap-y-4 py-5">
           <Link href={'/'}>
             <Image
               src={logo}
--- components/Socials.js
+++ components/Socials.js
@@ -3,14 +3,14 @@ import Link from "next/link";
 import { RiYoutubeLine, RiFacebookLine, RiLinkedinLine, RiGithubLine, RiArrowDownLine, RiArrowDropDownLine } from "react-icons/ri"
 
 const Socials = () => {
-  
+
   return (
-    <div className="flex items-center gap-x-5">
-      <a href="/resume.pdf" download='resume.pdf'  className='btn btn-sm border-none bg-accent text-white font-bold text-lg'><RiArrowDropDownLine />Resume</a>
-     
-      <Link href={'https://www.youtube.com/'} className="hover:text-accent transition-all duration-300">
-        <RiYoutubeLine className="text-xl" />
+    <div className="flex items-center gap-x-3 lg:gap-x-5">
+      <a href="/resume.pdf" download='resume.pdf' className='btn btn-sm border-none bg-accent text-white font-bold text-lg'><RiArrowDropDownLine />Resume</a>
+      <Link href={"/work"}>
+        <button className='btn btn-sm xl:hidden  bg-accent border-none text-white text-md'>Projects</button>
       </Link>
+
       <Link href={'https://www.facebook.com/ananta.asim.5/'} className="hover:text-accent transition-all duration-300">
         <RiFacebookLine className="text-xl" />
       </Link>

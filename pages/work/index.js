70d04acdfb82026fb088a5da611308c7449f55ef
shohaib1996 <khanshohaibhossain@gmail.com>
2023-12-11 22:26:11 +0600

    everything added

--- pages/work/index.js
+++ pages/work/index.js
@@ -8,31 +8,39 @@ import { fadeIn } from "../../variants"
 
 const Work = () => {
   return (
-    <div className="h-full bg-primary/30 py-36 flex items-center">
-      <Circles />
-      <div className="container mx-auto">
-        <div className="flex flex-col xl:flex-row gap-x-6">
-          <div className="text-center flex xl:w-[30vw] flex-col lg:text-left mb-4 xl:mb-0">
-            <motion.h2
-              variants={fadeIn("up", 0.2)} initial="hidden" animate="show" exit="hidden"
-              className="h2 xl:mt-6">
-              My Works
-            </motion.h2>
-            <motion.p
-              variants={fadeIn("up", 0.4)} initial="hidden" animate="show" exit="hidden"
-              className="mb-4 max-w-[400px] mx-auto lg:mx-0">As a MERN stack developer, I build some mern web application with the combination of frontend and backend. Hopefully you will checkout.
-            </motion.p>
+    <>
+      <div className="h-full bg-[#005c00]/60 py-36 flex items-center">
+        <Circles />
+        <div className="container mx-auto overflow-y-auto xl:overflow-y-hidden md:h-[250px] lg:h-[500px]">
+          <div className="flex flex-col xl:flex-row gap-x-6">
+            <div className="text-center flex xl:w-[30vw] flex-col lg:text-left mb-4 xl:mb-0 mt-0 xl:mt-12">
+              <motion.h2
+                variants={fadeIn("up", 0.2)} initial="hidden" animate="show" exit="hidden"
+                className="h2 mt-20 lg:mt-6">
+                My Works
+              </motion.h2>
+              <motion.p
+                variants={fadeIn("up", 0.4)} initial="hidden" animate="show" exit="hidden"
+                className="mb-4 max-w-[400px] mx-auto lg:mx-0">As a MERN stack developer, I build some mern web application with the combination of frontend and backend. Hopefully you will checkout.
+              </motion.p>
+            </div>
+            <motion.div
+              variants={fadeIn("down", 0.6)} initial="hidden" animate="show" exit="hidden"
+              className="w-full xl:max-w-[65%] mt-0 xl:mt-12">
+              <WorkSlider />
+            </motion.div>
           </div>
-          <motion.div 
-          variants={fadeIn("down", 0.6)} initial="hidden" animate="show" exit="hidden"
-          className="w-full xl:max-w-[65%]">
-            <WorkSlider />
-          </motion.div>
+          <footer className="footer footer-center p-2 bg-transparent text-white">
+            <aside>
+              <p className="text-white mt-0 lg:mt-20">Copyright © 2023 - All right reserved by <span >shohaib hossain</span></p>
+            </aside>
+          </footer>
         </div>
+        <Bulb />
       </div>
-      <Bulb />
 
-    </div>
+
+    </>
   )
 };
 
